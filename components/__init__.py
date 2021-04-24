5f4e2d89b35f8db7bd14bc26e7a2afdbc3551155
IvanNaum <63122936+IvanNaum@users.noreply.github.com>
2021-04-24 13:02:06 +0300

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -6,6 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 from components.checkers.session import Session
 
 app = Flask(__name__)
+app.debug = True
+
 app.secret_key = 'secret string'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -15,11 +17,15 @@ socket = SocketIO(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 
+from components import models
+
+db.create_all()
+
 from components.models import User
 
 # Список с информацией о игровых комнатах
 sessions = [
-    Session(User.query.filter_by(id=1).first(), User.query.filter_by(id=3).first())
+    Session(User.query.filter_by(id=1).first(), User.query.filter_by(id=2).first())
 ]
 
-from components import main_page, models, authorization, game
+from components import main_page, authorization, game
