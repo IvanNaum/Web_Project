391f2d4aa9f7cef65479c542066d6ec2de9c4193
ivan <nuk04@mail.ru>
2021-04-27 23:17:06 +0300

    test heroku

--- start.py
+++ start.py
@@ -1,4 +1,7 @@
+import os
+
 from components import app, socket
 
 if __name__ == '__main__':
-    socket.run(app=app)
+    port = int(os.environ.get("PORT", 5000))
+    socket.run(app=app, port=8000)
