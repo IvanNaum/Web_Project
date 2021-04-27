b54b5708c3f78c2838c93f6476850ea256c87cc0
Egor Masterskikh <65420211+egor-masterskikh@users.noreply.github.com>
2021-04-27 23:42:44 +0300

    add host for heroku

--- start.py
+++ start.py
@@ -4,4 +4,4 @@ from components import app, socket
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    socket.run(app=app, port=8000)
+    socket.run(app=app, host='0.0.0.0', port=8000)
